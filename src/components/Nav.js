a7cda193dea7644a9eeeece3d045ad65da952d98
JES20 <dore.joel.dore@gmail.com>
2021-02-27 09:20:11 -0500

    Add Nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
-const Nav = () => {
+export default function Nav() {
+    const location = useLocation();
+
     return (
-        <div>
-
-        </div>
-    )
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                    Home
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                    to="/story"
+                    className={location.pathname === "/story" ? "nav-link active" : "nav-link"}>
+                    Story
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                    to="/portfolio"
+                    className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>
+                    Portfolio
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                    to="/tech"
+                    className={location.pathname === "/tech" ? "nav-link active" : "nav-link"}>
+                    Tech Skills
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link
+                    to="/contact"
+                    className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
+                    Contact
+                </Link>
+            </li>
+        </ul>
+    );
 }
-
-export default Nav
