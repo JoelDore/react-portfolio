2c8798b8a42776558a8ecf7ee2a967c42d23a818
JES20 <dore.joel.dore@gmail.com>
2021-02-27 09:21:05 -0500

    Add Nav to app with React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Landing from "./pages/Landing";
+import Story from "./pages/Story";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div>
-      React Portfolio
-    </div>
+    <>
+      <Router>
+        <Nav />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/story" component={Story} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+      </Router>
+    </>
   );
 }
 
