fdf478ec6e5c0662b15f5349f2f7b4a8a33620c3
JES20 <dore.joel.dore@gmail.com>
2021-02-28 14:08:47 -0500

    Remove unused div tag and css variables

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,26 +6,24 @@ import './header.css'
 
 const Header = () => {
     return (
-        <div>
-            <Navbar fixed="top" expand="sm" variant="light" bg="transparent">
-                <Container fluid>
-                    <Navbar.Brand as="div">
-                        <img
-                            src={profileImg}
-                            width="40"
-                            height="40"
-                            alt="Profile"
-                            className="d-inline-block align-top rounded-circle"
-                        />
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navLinks" />
-                    <Navbar.Collapse id="navLinks">
-                        <Nav />
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+        <Navbar fixed="top" expand="sm" variant="light" bg="transparent">
+            <Container fluid>
+                <Navbar.Brand as="div">
+                    <img
+                        src={profileImg}
+                        width="40"
+                        height="40"
+                        alt="Profile"
+                        className="d-inline-block align-top rounded-circle"
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navLinks" />
+                <Navbar.Collapse id="navLinks">
+                    <Nav />
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
 
-        </div>
     )
 }
 
