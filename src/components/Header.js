dd26f428e60e17b77f9fb232ffe8c4aea1976a41
JES20 <dore.joel.dore@gmail.com>
2021-02-28 12:33:58 -0500

    Build Navbar component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import Nav from './Nav'
+import { Container, Navbar } from 'react-bootstrap'
+import profileImg from '../assets/images/profile.jpg'
+import './header.css'
 
 const Header = () => {
     return (
         <div>
+            <Navbar fixed="top" expand="sm" variant="light" bg="transparent">
+                <Container fluid>
+                    <Navbar.Brand as="div">
+                        <img
+                            src={profileImg}
+                            width="40"
+                            height="40"
+                            alt="Profile"
+                            className="d-inline-block align-top rounded-circle"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navLinks" />
+                    <Navbar.Collapse id="navLinks">
+                        <Nav />
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
 
         </div>
     )
 }
+
+
 
 export default Header
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,44 +1,34 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-
+import BootNav from 'react-bootstrap/Nav'
 export default function Nav() {
     const location = useLocation();
 
     return (
-        <ul className="nav nav-tabs">
-            <li className="nav-item">
-                <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
-                    Home
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                    to="/story"
-                    className={location.pathname === "/story" ? "nav-link active" : "nav-link"}>
-                    Story
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                    to="/portfolio"
-                    className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>
-                    Portfolio
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                    to="/tech"
-                    className={location.pathname === "/tech" ? "nav-link active" : "nav-link"}>
-                    Tech Skills
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                    to="/contact"
-                    className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
-                    Contact
-                </Link>
-            </li>
-        </ul>
+        <BootNav className="ms-auto my-2 mb-md-0 px-3 text-end">
+            <Link to="/" className={`${location.pathname === "/" ? "nav-link active" : "nav-link"} nav-item`}>
+                Home
+            </Link>
+            <Link
+                to="/story"
+                className={location.pathname === "/story" ? "nav-link active" : "nav-link"}>
+                Story
+            </Link>
+            <Link
+                to="/portfolio"
+                className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>
+                Portfolio
+            </Link>
+            <Link
+                to="/tech"
+                className={location.pathname === "/tech" ? "nav-link active" : "nav-link"}>
+                Tech Skills
+            </Link>
+            <Link
+                to="/contact"
+                className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
+                Contact
+            </Link>
+        </BootNav >
     );
 }
