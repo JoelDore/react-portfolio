7a20602bbbc0703bf20e765c712375afc7f45634
JES20 <dore.joel.dore@gmail.com>
2021-02-28 15:32:37 -0500

    Create Landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,10 @@
 import React from 'react'
+// import { Image } from 'react-bootstrap'
+import bgImg from '../assets/images/lake-clark-bg.jpg'
+import './landing.css'
 
 export default function Landing() {
     return (
-        <div>
-
-        </div>
+        <section className="landing-wrapper" style={{ backgroundImage: `url(${bgImg})` }}></section>
     )
 }
