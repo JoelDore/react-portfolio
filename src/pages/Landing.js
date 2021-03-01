6a91d8bb69c44802e54fe6de499a8479238ea014
JES20 <dore.joel.dore@gmail.com>
2021-03-01 09:47:42 -0500

    Add text to Landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,12 @@
 import React from 'react'
-// import { Image } from 'react-bootstrap'
 import bgImg from '../assets/images/lake-clark-bg.jpg'
 import './landing.css'
 
 export default function Landing() {
     return (
-        <section className="landing-wrapper" style={{ backgroundImage: `url(${bgImg})` }}></section>
+        <section className="landing-wrapper" style={{ backgroundImage: `url(${bgImg})` }}>
+            <h1>Joel Dore</h1>
+            <h2>Web Developer</h2>
+        </section>
     )
 }
