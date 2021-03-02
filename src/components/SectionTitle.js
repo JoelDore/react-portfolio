87bd5c2ab8b30cf3f4aee8f8ef432b8854168de3
JES20 <dore.joel.dore@gmail.com>
2021-03-02 10:04:47 -0500

    Change margin on SectionTitle component

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function SectionTitle({ title }) {
     return (
-        <div className="mb-5">
+        <div className="my-5">
             <h1>{title}</h1>
         </div>
     )
