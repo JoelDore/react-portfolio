a3b2655cae13c0ac4baea3c7d71ce3c00df5070e
JES20 <dore.joel.dore@gmail.com>
2021-03-02 09:53:31 -0500

    Change navbar text color based on page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 import Nav from './Nav'
 import { Container, Navbar } from 'react-bootstrap'
 import profileImg from '../assets/images/profile.jpg'
 import './header.css'
 
 const Header = () => {
+    const location = useLocation()
     return (
-        <Navbar fixed="top" expand="sm" variant="light" bg="transparent">
+        <Navbar fixed="top" expand="sm" variant={location.pathname === "/" ? "light" : "dark"} bg="transparent">
             <Container fluid>
                 <Navbar.Brand as="div">
                     <img
