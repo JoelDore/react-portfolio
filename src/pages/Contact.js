55deeed0f1069a1739d8522b41106b7e8ded670b
JES20 <dore.joel.dore@gmail.com>
2021-03-03 12:49:09 -0500

    Fix bootstrap imports in Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
-import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import SectionTitle from '../components/SectionTitle'
 
